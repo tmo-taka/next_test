5e1b5dba7f7b6c64c30bb70b9428f0d56303634f
takahara tomoya <t.tkorock221@gmail.com>
2022-04-30 21:23:27 +0900

    useMomeマスター

--- next_test/pages/dynamic/[[...slug]].tsx
+++ next_test/pages/dynamic/[[...slug]].tsx
@@ -1,8 +1,33 @@
+import React, { useMemo, useState } from "react";
 import { useRouter } from 'next/router';
 
+const highCostProcessing = (inputValue: number) => {
+    let caliculateValue = Number(inputValue);
+    for (let i = 0; i < 1000; i++) {
+        caliculateValue += 1;
+    }
+    console.log("Finished");
+    return caliculateValue;
+};
+
 export default function Home() {
+    const [value, setValue] = useState(0);
+    const [count, setCount] = useState(0);
     const path = useRouter();
+
+    // const memorizedValue = (() => {
+    //     return highCostProcessing(value);
+    // });
+
+    const memorizedValue = useMemo(() => {
+        return highCostProcessing(value);
+    }, [value]);
+
     return (
-        <div>{path.asPath}</div>
+        <>
+            <div>{memorizedValue}</div>
+            <button onClick={() => setCount(count + 1)}>プラス1</button>
+            <div>{path.asPath}</div>
+        </>
     )
 }
